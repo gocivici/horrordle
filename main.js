c453d367cbf80222b2e23dad05c568d67a4412b7
gocivici <gocivici@gmail.com>
2022-09-29 00:03:17 +0300

    fixed date bug

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ function revealButtons(g = guessNo){
   var movies = [["The Texas Chainsaw Massacre",1,"Sally Hardesty"],["It Follows",2,"Jaime Height"],["Friday the 13th",0,"Alice Hardy"],["Event Horizon",2,"Lieutenant Starck"],["Hellraiser",2,"Kirsty Cotton"],["A Nightmare on Elm Street",1,"Nancy Thompson"],["The Cabin in the Woods",2],["American Psycho",0]];
   var pix = [];
   // autocomplete(document.getElementById("guess"), movies);
-  var gameBeginning = new Date('September 22, 2022 00:00:00');
+  var gameBeginning = new Date('September 24, 2022 00:00:00');
   var countDownTime =  new Date();
   var present_date = new Date();
 
