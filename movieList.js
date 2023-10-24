fee4cf6728dbc431d9170fecc38293ffb0508129
gocivici <gocivici@gmail.com>
2023-10-24 20:42:31 +0300

    movielist update

--- movieList.js
+++ movieList.js
@@ -23,6 +23,7 @@ var movies = [
     ["Smile",0,"Joel"],
     ["Get Out",2,"Chris Washington"],
     ["Hellraiser",2,"Kirsty Cotton"],
+    ["Mirrors",3,"No survivors"],
     ["Friday the 13th"],
     ["Saw"],
     ["The Ring"],
